a3ff663de8f2e5f54bbda57766f86f31ef1b87ef
James Murray <jem12475@gmail.com>
2025-01-27 20:20:08 -0500

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jembbn/puppy
 
 go 1.22.1
+
+require github.com/jembbn/dog v0.0.0-20250128010921-9d3033bca9e3
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import (
+	"github.com/jembbn/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
